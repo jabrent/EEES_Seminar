1a74f123b515811aed9f6ccaa064c5513fe2ec8b
jabrent <jennie.brentrup@gmail.com>
2018-03-29 17:20:58 -0400

    Clean-up script for new tests

--- R Code/Data_Manipulation.R
+++ R Code/Data_Manipulation.R
@@ -9,21 +9,5 @@ tidyverse_packages()
 library(hms)
 
 # Add comments below here to try using git actions ####
-<<<<<<< HEAD
-b <- 6+2
-(vector1 <- c(2,4,6)) 
-vector1
-=======
-<<<<<<< HEAD
-b <- 6+2
-=======
-<<<<<<< HEAD
-
-b <-  6+2
 
 # Braden testing push #
-=======
-b <- 6+2
->>>>>>> 85e406fcd97103b4ad4d376f0436208276ad76b5
->>>>>>> 776d89ffc46a757d89ee46ec39308409973ffb2e
->>>>>>> 9bf686a6b0c354a99c09026056d7f70c56ee8608
