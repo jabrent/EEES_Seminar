4110e020929260572662e8134e484766ad96a36b
mepeach <Morgan.E.Peach.GR@dartmouth.edu>
2018-04-04 11:47:33 -0400

    Morgan is amending script to match master on github

--- R Code/Data_Manipulation.R
+++ R Code/Data_Manipulation.R
@@ -8,16 +8,5 @@ tidyverse_packages()
 
 library(hms)
 
-# Add comments below here to try using git actions ####
-<<<<<<< HEAD
-b <- 6+2
-=======
-<<<<<<< HEAD
 
-b <-  6+2
 
-# Braden testing push #
-=======
-b <- 6+2
->>>>>>> 85e406fcd97103b4ad4d376f0436208276ad76b5
->>>>>>> 776d89ffc46a757d89ee46ec39308409973ffb2e
