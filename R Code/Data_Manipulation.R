4d0004b8447a1357f61296492d62743702431a37
lizstuder <lizzy.studer@gmail.com>
2018-04-05 14:14:56 -0400

    Liz test

--- R Code/Data_Manipulation.R
+++ R Code/Data_Manipulation.R
@@ -9,8 +9,9 @@ tidyverse_packages()
 library(hms)
 
 # Add comments below here to try using git actions ####
-<<<<<<< HEAD
-Liz <- 5+25+88
-=======
-c <- 1+1
->>>>>>> e249a431780a6b1b1318654a24389ff2fe370945
+
+# Braden testing push #
+
+# Morgan testing push #
+
+# Clare testing push #
